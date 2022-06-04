9c98acb60ed66c3bbc31d217a5491a62af77b4d6
DeveloperInProgress <velnaveen99@gmail.com>
2022-06-04 16:14:47 +0000

    add arweave project

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,55 +1,20 @@
-import { Block, Message, Transaction, TransferEvent } from "../types";
+import { Block, Transaction } from "../types";
 import {
-  CosmosEvent,
-  CosmosBlock,
-  CosmosMessage,
-  CosmosTransaction,
-} from "@subql/types-cosmos";
+  ArBlock,
+  ArTransaction
+} from "@subql/types-arweave";
 
-export async function handleBlock(block: CosmosBlock): Promise<void> {
-
+export async function handleBlock(block: ArBlock): Promise<void> {
+  const record = new Block(block.block.hash);
+  record.id = block.block.hash;
+  record.height = BigInt(block.block.height);
 }
 
-export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
+export async function handleTransaction(tx: ArTransaction): Promise<void> {
   const record = new Transaction(tx.tx.hash);
-  record.blockHeight = BigInt(tx.block.block.header.height);
-  record.timestamp = tx.block.block.header.time;
-  await record.save();
-}
-
-export async function handleMessage(
-  msg: CosmosMessage
-): Promise<void> {
-  const record = new Message(`${msg.tx.tx.hash}-${msg.idx}`);
-  record.blockHeight = BigInt(msg.block.block.header.height);
-  record.txHash = msg.tx.tx.hash;
-  record.fromAddress = msg.msg.fromAddress;
-  record.toAddress = msg.msg.toAddress;
-  record.amount = JSON.stringify(msg.msg.amount);
-  await record.save();
-}
-
-export async function handleEvent(
-  event: CosmosEvent
-): Promise<void> {
-  const record = new TransferEvent(
-    `${event.tx.tx.hash}-${event.msg.idx}-${event.idx}`
-  );
-  record.blockHeight = BigInt(event.block.block.header.height);
-  record.txHash = event.tx.tx.hash;
-  for (const attr of event.event.attributes) {
-    switch (attr.key) {
-      case "sender":
-        record.sender = attr.value;
-        break;
-      case "recipient":
-        record.recipient = attr.value;
-        break;
-      case "amount":
-        record.amount = attr.value;
-        break;
-      default:
-    }
-  }
+  record.blockHeight = BigInt(tx.block.block.height);
+  record.timestamp = tx.block.block.timestamp.toString();
+  record.tags = tx.tags.toString();
+  record.data = tx.tags.toString();
   await record.save();
 }
